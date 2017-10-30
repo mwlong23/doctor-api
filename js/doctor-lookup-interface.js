7d7f13ad91935fc562af3a1d58abc603d05eadb1
Mitchell <meechllada@gmail.com>
2017-10-29 19:05:39 -0700

    fix search by symptom

--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
     if(searchCriteria !== ""){
       $('#blank-search-field').hide();
       let baseUrl = `https://api.betterdoctor.com/2016-03-01/doctors?location=47.606,-122.332,10&skip=2&limit=10&user_key=${apiKey}`;
+      
 
       let promise = new DoctorSearch
       let searchResults = promise.search(baseUrl, category, searchCriteria)
